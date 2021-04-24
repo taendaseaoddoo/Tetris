b05e887b80c6805ea7b8a90a43e49e2b1357709b
taendaseaoddoo <luanhe19783ty@126.com>
2021-04-24 17:22:13 +0800

    start

--- dist/js/app.js
+++ dist/js/app.js
@@ -28,62 +28,91 @@ require([
     './music',
     './defaults'
 ], function ($, _, Backbone, Tetris, Music, defaults) {
-    var template = '';
-    var tetris_model = Backbone.Model.extend({
-        default: {
 
-        }
-    });
-    var tetris_collection = Backbone.Collection.extend({
-        model: tetris_model
-    });
-    var tetirs_router = Backbone.Router.extend({
+    var app = app || {};
+
+    app.Router = Backbone.Router.extend({
         routes: {
+            '': 'index',
+            '/music': 'getMusic',
+            '/tetris': 'getTetris'
+        },
+        index: function () {
+
+        },
+        getMusic: function () {
+
+        },
+        getTetris: function () {
 
         }
     });
-    var tetirs_view = Backbone.View.extend({
-        el: '#tetris-demo',
-        template: _.template(template),
-        events: {
+    app.Model = Backbone.Model.extend({
+        initialize: function () {
+            console.log('app model initialize');
+        },
+        default: {
+            title: '',
+            completed: false
+        },
+        toggle: function () {
             
+        }
+    });
+    app.Collection = Backbone.Collection.extend({
+        model: app.Model
+    });
+
+    app.View = Backbone.View.extend({
+        el: '#app',
+        template: _.template($('#app-template').html()),
+        events: {
+            'click #app': 'bodyClick'
         },
         initialize: function () {
+            console.log('app view initialize');
         },
         render: function () {
+            console.log('app view render');
+        },
+        bodyClick: function () {
         }
     });
+
+    var appCollection = new app.Collection;
+    var appRouter = new app.Router();
+    var appView = new app.View();
     Backbone.history.start();
 
-    $.fn.tetris = function (options1, options2) {
-        var tetris_settings = $.extend({}, defaults.tetris, options1);
-        var music_settings = $.extend({}, defaults.music, options2);
-        return this.each(function (index, element) {
-            var tetris = new Tetris(tetris_settings, $(element));
-            tetris.init();
-            var music = new Music(music_settings);
-            music.init();
-            music.play();
-        });
-    };
-
-    $('#tetris-demo').css({
-        'width': $(window).width(),
-        'height': $(window).height()
-    })
-    .tetris({
-        speed: 20,
-        autoplay: false,
-        autoplayRestart: false
-    }, {
-        holder: '#tetris-demo',
-        random: true,
-        paths: '../mp3/',
-        playlist: [
-            'm1.mp3',
-            'm2.mp3',
-            'm3.mp3',
-            'm4.mp3'
-        ]
-    });
+    // $.fn.tetris = function (options1, options2) {
+    //     var tetris_settings = $.extend({}, defaults.tetris, options1);
+    //     var music_settings = $.extend({}, defaults.music, options2);
+    //     return this.each(function (index, element) {
+    //         var tetris = new Tetris(tetris_settings, $(element));
+    //         tetris.init();
+    //         var music = new Music(music_settings);
+    //         music.init();
+    //         music.play();
+    //     });
+    // };
+    //
+    // $('#tetris-demo').css({
+    //     'width': $(window).width(),
+    //     'height': $(window).height()
+    // })
+    // .tetris({
+    //     speed: 20,
+    //     autoplay: false,
+    //     autoplayRestart: false
+    // }, {
+    //     holder: '#tetris-demo',
+    //     random: true,
+    //     paths: '../mp3/',
+    //     playlist: [
+    //         'm1.mp3',
+    //         'm2.mp3',
+    //         'm3.mp3',
+    //         'm4.mp3'
+    //     ]
+    // });
 });
